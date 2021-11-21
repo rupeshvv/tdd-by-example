53c8ac7830334e9f6476d2237686008714a93daa
Rupesh <rupeshbabuvv@gmail.com>
2021-11-21 17:38:14 +0530

    multi-currency money Degenerate objects equality for all

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+public class Dollar {
+       int amount;
+       public Dollar(int amount){
+           this.amount =amount;
+       }
+       Dollar times(int multiplier){
+         return new Dollar(amount * multiplier);
+       }
+       public boolean equals(Object obj){
+           Dollar dollar=(Dollar)obj;
+           return  this.amount == dollar.amount;
+       }
+}
