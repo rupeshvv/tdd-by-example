fd8f213d7cfef9cba25e133bd661c6b8a29f7e6e
Rupesh <rupeshbabuvv@gmail.com>
2021-11-23 19:20:23 +0530

    testSimpleAddition()

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -11,27 +11,20 @@ public class MoneyTest {
 
 
     @Test
-    void testMultiplicationDollar() {
+    void testMultiplicationMoney() {
         Money five= Money.dollar(5);
         assertEquals(Money.dollar(10),five.times(2));
         assertEquals(Money.dollar(15),five.times(3));
+
+        Money fiveF=Money.franc(5);
+        assertEquals(Money.franc(10),fiveF.times(2));
+
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEqualityMoney() {
         assertEquals(Money.dollar(5),Money.dollar(5));
         assertNotEquals(Money.dollar(5),Money.dollar(8));
-    }
-
-    @Test
-    void testMultiplicationFranc() {
-        Money five=Money.franc(5);
-        assertEquals(Money.franc(10),five.times(2));
-        assertEquals(Money.franc(15),five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         assertEquals(Money.franc(5),Money.franc(5));
         assertNotEquals(Money.franc(5),Money.franc(8));
     }
@@ -40,6 +33,15 @@ public class MoneyTest {
     void testCurrency() {
         assertEquals("USD",Money.dollar(1).currency());
         assertEquals("CHF",Money.franc(1).currency());
+    }
 
+
+    @Test
+    void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
